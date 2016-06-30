e0441305395d09ef1591e5558f6bf24dc778fe17
Clayton Mentzer <clayton.mentzer@onyxpoint.com>
2016-06-30 12:15:46 -0400

    fixed file extension

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,7 @@ class dnsmasq {
   file { '/etc/dnsmasq.conf':
     ensure  => file,
     require => Package['dnsmasq'],
-    content  => epp('dnsmasq/dnsmasq.conf.erb')
+    content => epp('dnsmasq/dnsmasq.conf.epp', 
+    { 'spoofed_addresses' =>  {"alice.com", "bob.com" }})
   }
 }
