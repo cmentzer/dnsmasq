f919d8b1410e02b43146371de99f7274feefb163
Clayton Mentzer <clayton.mentzer@onyxpoint.com>
2016-06-30 13:51:24 -0400

    testing

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,6 +27,7 @@ class dnsmasq (
   # ensure that the config file is present and provide defaults 
   # for the spoofed hostnames.
   file { "$config_path":
+    notify{"the value of spoofed sites is ${spoofed_sites}":}
     ensure  => file,
     require => Package['dnsmasq'],
     content => epp($config_template,
