07b20e9eab5d5da3a83d9422c9e2cae16cf4db3c
Clayton Mentzer <clayton.mentzer@onyxpoint.com>
2016-06-30 10:58:07 -0400

    fixed path

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,6 @@ class dnsmasq {
   file { '/etc/dnsmasq.conf':
     ensure  => file,
     require => Package['dnsmasq'],
-    content  => template('puppet:///modules/dnsmasq/dnsmasq.conf.erb')
+    content  => template('dnsmasq/dnsmasq.conf.erb')
   }
 }
